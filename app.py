164aa21d201e68bd162be2a3f75947871b2dca80
Pjjames95 <gachombajames7@gmail.com>
2024-10-23 16:03:54 +0300

    python practice

---
+++ app.py
@@ -0,0 +1,22 @@
+from operator import truth
+
+num1 = 1
+num2 = 2.0
+firstname = "James"
+secondname = "Gachomba"
+#sequence
+#list
+fruits = ["apple", "orange","mango"]
+#tuple
+fruitstuple = ("apple", "orange", "mango")
+#dictionary
+foods = {
+    "proteins" : ["meat", "beans"],
+"carbohydrates" : ("maize", "chapati"),
+"vitamins": "fruits"
+}
+#boolean
+result = True
+
+print("James Gachomba")
+
